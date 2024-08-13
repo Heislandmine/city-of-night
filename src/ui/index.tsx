0abd7db38dd76f44537913b490597cca42fc3461
heislandmine <heislandmine666@protonmail.com>
2024-08-13 17:03:05 +0900

    アクションメニュー画面の初期実装

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,10 +1,43 @@
 import React from "react";
-import { Box, render, Text } from "ink";
+import { Box, render, Spacer, Text } from "ink";
 
+const TopStatusBar = () => {
+  return (
+    <Box borderStyle={"single"} width={"100%"}>
+      <Text>2124/4/1</Text>
+      <Spacer />
+      <Text>(昼)</Text>
+      <Spacer />
+      <Text>10日目</Text>
+      <Spacer />
+      <Text>残り200日</Text>
+    </Box>
+  );
+};
+
+const TopActionMenu = ({
+  menuItems,
+}: {
+  menuItems: { callNumber: number; displayName: string }[];
+}) => {
+  return (
+    <Box width={"100%"} borderStyle={"classic"} gap={2}>
+      {menuItems.map((e) => (
+        <Text key={e.displayName}>{`[${e.callNumber}]${e.displayName}`}</Text>
+      ))}
+    </Box>
+  );
+};
 const App = () => {
   return (
-    <Box borderStyle={"single"}>
-      <Text>hello</Text>
+    <Box width={"100%"} flexDirection="column">
+      <TopStatusBar></TopStatusBar>
+      <TopActionMenu
+        menuItems={[
+          { callNumber: 100, displayName: "調教" },
+          { callNumber: 200, displayName: "保存" },
+        ]}
+      />
     </Box>
   );
 };
