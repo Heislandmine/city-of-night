441f3ba2a11afc740bb9abafcddbc86a57a466ae
heislandmine <heislandmine666@protonmail.com>
2024-08-22 05:25:53 +0900

    描画処理をappモジュールに移動

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,10 @@
+use ratatui::{
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{Color, Style},
+    widgets::{Block, BorderType, Borders, Paragraph},
+    Frame,
+};
+
 pub struct App {
     pub should_quit: bool,
 }
@@ -9,5 +16,89 @@ impl App {
 
     pub fn quit(&mut self) {
         self.should_quit = true;
+    }
+
+    pub fn render(&self, frame: &mut Frame) {
+        self.render_main_ui(frame);
+    }
+
+    fn render_main_ui(&self, frame: &mut Frame) {
+        let area = frame.area();
+
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(vec![Constraint::Max(3), Constraint::Percentage(80)])
+            .split(area);
+
+        self.render_top_bar(frame, layout[0]);
+
+        frame.render_widget(
+            Block::new()
+                .border_type(BorderType::Plain)
+                .borders(Borders::all())
+                .border_style(Style::default().fg(Color::White)),
+            layout[1],
+        );
+
+        let command_area_raws = Layout::new(
+            Direction::Vertical,
+            vec![
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+            ],
+        )
+        .split(layout[1]);
+
+        let command_area_first_row = Layout::new(
+            Direction::Horizontal,
+            vec![
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+            ],
+        )
+        .margin(1)
+        .split(command_area_raws[0]);
+
+        frame.render_widget(
+            Paragraph::new("[100]調教").centered(),
+            command_area_first_row[0],
+        );
+
+        frame.render_widget(
+            Paragraph::new("[200]セーブ").centered(),
+            command_area_first_row[1],
+        );
+
+        frame.render_widget(
+            Paragraph::new("[300]ロード").centered(),
+            command_area_first_row[2],
+        );
+    }
+
+    fn render_top_bar(&self, frame: &mut Frame, area: Rect) {
+        let top_bar_layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+                Constraint::Percentage(25),
+            ])
+            .margin(1)
+            .split(area);
+        frame.render_widget(
+            Block::new()
+                .border_type(BorderType::Plain)
+                .borders(Borders::all()),
+            area,
+        );
+        frame.render_widget(Paragraph::new("2124/8/5").centered(), top_bar_layout[0]);
+        frame.render_widget(Paragraph::new("昼").centered(), top_bar_layout[1]);
+        frame.render_widget(Paragraph::new("10日目").centered(), top_bar_layout[2]);
+        frame.render_widget(Paragraph::new("残り200日").centered(), top_bar_layout[3]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,11 @@
-use std::io::{self, Stdout};
+use std::io::{self};
 
 use app::App;
-use ratatui::{
-    crossterm::{
-        event::{self, DisableMouseCapture, EnableMouseCapture, KeyCode, KeyEventKind},
-        execute,
-        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    },
-    layout::{Constraint, Direction, Layout, Rect},
-    prelude::{Backend, CrosstermBackend},
-    style::{Color, Style},
-    widgets::{Block, BorderType, Borders, Paragraph},
-    CompletedFrame, Frame, Terminal,
-};
+use ratatui::crossterm::event::{self, KeyCode, KeyEventKind};
 use tui::Tui;
 
 mod app;
 mod tui;
-
-fn render_top_bar(frame: &mut Frame, area: Rect) {
-    let top_bar_layout = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(vec![
-            Constraint::Percentage(25),
-            Constraint::Percentage(25),
-            Constraint::Percentage(25),
-            Constraint::Percentage(25),
-        ])
-        .margin(1)
-        .split(area);
-    frame.render_widget(
-        Block::new()
-            .border_type(BorderType::Plain)
-            .borders(Borders::all()),
-        area,
-    );
-    frame.render_widget(Paragraph::new("2124/8/5").centered(), top_bar_layout[0]);
-    frame.render_widget(Paragraph::new("昼").centered(), top_bar_layout[1]);
-    frame.render_widget(Paragraph::new("10日目").centered(), top_bar_layout[2]);
-    frame.render_widget(Paragraph::new("残り200日").centered(), top_bar_layout[3]);
-}
-
-fn render_main_ui(frame: &mut Frame) {
-    let area = frame.area();
-
-    let layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(vec![Constraint::Max(3), Constraint::Percentage(80)])
-        .split(area);
-
-    render_top_bar(frame, layout[0]);
-
-    frame.render_widget(
-        Block::new()
-            .border_type(BorderType::Plain)
-            .borders(Borders::all())
-            .border_style(Style::default().fg(Color::White)),
-        layout[1],
-    );
-
-    let command_area_raws = Layout::new(
-        Direction::Vertical,
-        vec![
-            Constraint::Percentage(25),
-            Constraint::Percentage(25),
-            Constraint::Percentage(25),
-            Constraint::Percentage(25),
-        ],
-    )
-    .split(layout[1]);
-
-    let command_area_first_row = Layout::new(
-        Direction::Horizontal,
-        vec![
-            Constraint::Percentage(25),
-            Constraint::Percentage(25),
-            Constraint::Percentage(25),
-            Constraint::Percentage(25),
-        ],
-    )
-    .margin(1)
-    .split(command_area_raws[0]);
-
-    frame.render_widget(
-        Paragraph::new("[100]調教").centered(),
-        command_area_first_row[0],
-    );
-
-    frame.render_widget(
-        Paragraph::new("[200]セーブ").centered(),
-        command_area_first_row[1],
-    );
-
-    frame.render_widget(
-        Paragraph::new("[300]ロード").centered(),
-        command_area_first_row[2],
-    );
-}
 
 fn main() -> io::Result<()> {
     let mut tui = Tui::new()?;
@@ -105,7 +14,7 @@ fn main() -> io::Result<()> {
 
     while !app.should_quit {
         tui.draw(|frame| {
-            render_main_ui(frame);
+            app.render(frame);
         })?;
 
         if event::poll(std::time::Duration::from_millis(16))? {
