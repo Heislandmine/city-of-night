1946b891069625dba3a896394e33c14b8e23c56f
heislandmine <heislandmine666@protonmail.com>
2024-08-22 12:25:52 +0900

    home画面の描画をhomeモジュールに移動

--- src/app.rs
+++ src/app.rs
@@ -2,21 +2,23 @@ use std::io;
 
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEventKind},
-    layout::{Constraint, Direction, Layout, Rect},
-    style::{Color, Style},
-    widgets::{Block, BorderType, Borders, Paragraph},
     Frame,
 };
 
+use crate::home::Home;
 use crate::tui::Tui;
 
 pub struct App {
     pub should_quit: bool,
+    component: Home,
 }
 
 impl App {
-    pub fn new() -> Self {
-        Self { should_quit: false }
+    pub fn new(component: Home) -> Self {
+        Self {
+            should_quit: false,
+            component,
+        }
     }
 
     pub fn quit(&mut self) {
@@ -24,87 +26,7 @@ impl App {
     }
 
     pub fn render(&self, frame: &mut Frame) {
-        self.render_main_ui(frame);
-    }
-
-    fn render_main_ui(&self, frame: &mut Frame) {
-        let area = frame.area();
-
-        let layout = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints(vec![Constraint::Max(3), Constraint::Percentage(80)])
-            .split(area);
-
-        self.render_top_bar(frame, layout[0]);
-
-        frame.render_widget(
-            Block::new()
-                .border_type(BorderType::Plain)
-                .borders(Borders::all())
-                .border_style(Style::default().fg(Color::White)),
-            layout[1],
-        );
-
-        let command_area_raws = Layout::new(
-            Direction::Vertical,
-            vec![
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-            ],
-        )
-        .split(layout[1]);
-
-        let command_area_first_row = Layout::new(
-            Direction::Horizontal,
-            vec![
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-            ],
-        )
-        .margin(1)
-        .split(command_area_raws[0]);
-
-        frame.render_widget(
-            Paragraph::new("[100]調教").centered(),
-            command_area_first_row[0],
-        );
-
-        frame.render_widget(
-            Paragraph::new("[200]セーブ").centered(),
-            command_area_first_row[1],
-        );
-
-        frame.render_widget(
-            Paragraph::new("[300]ロード").centered(),
-            command_area_first_row[2],
-        );
-    }
-
-    fn render_top_bar(&self, frame: &mut Frame, area: Rect) {
-        let top_bar_layout = Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints(vec![
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-            ])
-            .margin(1)
-            .split(area);
-        frame.render_widget(
-            Block::new()
-                .border_type(BorderType::Plain)
-                .borders(Borders::all()),
-            area,
-        );
-        frame.render_widget(Paragraph::new("2124/8/5").centered(), top_bar_layout[0]);
-        frame.render_widget(Paragraph::new("昼").centered(), top_bar_layout[1]);
-        frame.render_widget(Paragraph::new("10日目").centered(), top_bar_layout[2]);
-        frame.render_widget(Paragraph::new("残り200日").centered(), top_bar_layout[3]);
+        self.component.render(frame)
     }
 
     pub fn handle_events(&mut self) -> io::Result<()> {
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use app::App;
+use home::Home;
 use std::io::{self};
 use tui::Tui;
 
 mod app;
+mod home;
 mod tui;
 
 fn main() -> io::Result<()> {
     let tui = Tui::new()?;
-    let mut app = App::new();
+    let home = Home::new();
+    let mut app = App::new(home);
 
     app.run(tui)?;
 
