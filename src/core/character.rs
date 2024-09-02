daeb5f918399039d4fbfd43388d92a57b71706c9
heislandmine <heislandmine666@protonmail.com>
2024-09-03 05:50:10 +0900

    Characterにmax_hpとcurrent_hpのゲッターを実装

--- src/core/character.rs
+++ src/core/character.rs
@@ -3,6 +3,7 @@ pub struct Character {
     id: String,
     display_name: String,
     base_hp: u16,
+    current_hp: u16,
 }
 
 impl Character {
@@ -11,6 +12,7 @@ impl Character {
             id: id.into(),
             display_name: display_name.into(),
             base_hp,
+            current_hp: base_hp,
         }
     }
 
@@ -20,5 +22,13 @@ impl Character {
 
     pub fn display_name(&self) -> String {
         self.display_name.clone()
+    }
+
+    pub fn max_hp(&self) -> u16 {
+        self.base_hp
+    }
+
+    pub fn current_hp(&self) -> u16 {
+        self.current_hp
     }
 }
